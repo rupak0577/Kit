5ccd6625942eb12f50e160ed664ddc99d2efb005
Rupak <rupakdas.dev@gmail.com>
2024-11-10 18:02:24 +0530

    set up dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    application
+}
+
+application {
+    mainClass = "org.example.MainKt"
 }
 
 group = "org.example"
@@ -11,6 +16,9 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("com.github.ajalt.clikt:clikt:5.0.1")
+    implementation("org.ini4j:ini4j:0.5.4")
 }
 
 tasks.test {
